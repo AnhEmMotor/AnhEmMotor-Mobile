0f6957d4231a686c41827ef9e8c8696d63a8414e
Trần Thanh Bình <tranthanhbinh.thcsthanhson@gmail.com>
2026-08-10 10:11:32 +0700

    feat: remove eslint ignore config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,25 +6,5 @@ module.exports = {
 		node: true,
 		es2021: true,
 	},
-	plugins: ['unused-imports'],
-	rules: {
-		'no-unused-vars': 'off',
-		'unused-imports/no-unused-imports': 'warn',
-		'unused-imports/no-unused-vars': [
-			'warn',
-			{ 'vars': 'all', 'varsIgnorePattern': '^_', 'args': 'after-used', 'argsIgnorePattern': '^_' }
-		],
-		'no-undef': 'off',
-		'no-var': 'off',
-		'no-unused-expressions': 'off',
-		'import/no-extraneous-dependencies': 'off',
-		'import/prefer-default-export': 'off',
-		'react/react-in-jsx-scope': 'off',
-		'react/prop-types': 'off',
-		'react/display-name': 'off',
-		'import/namespace': 'off',
-		'import/no-unresolved': 'off',
-		'no-console': 'off',
-		'react-hooks/exhaustive-deps': 'warn',
-	},
+	plugins: ["unused-imports"],
 };
